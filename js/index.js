e9cf2467d8c0508e5c5619419080e8728a21b646
ardi2924 <indrardi92@gmail.com>
2025-07-16 21:31:24 +0700

    19th commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 function checkDate() {
-  const dateInput = document.getElementById("dateInput").value;
+  const d1 = document.getElementById("d1").value;
+  const d2 = document.getElementById("d2").value;
+  const m1 = document.getElementById("m1").value;
+  const m2 = document.getElementById("m2").value;
+  const y1 = document.getElementById("y1").value;
+  const y2 = document.getElementById("y2").value;
+  const y3 = document.getElementById("y3").value;
+  const y4 = document.getElementById("y4").value;
+
+  const inputDate = `${y1}${y2}${y3}${y4}-${m1}${m2}-${d1}${d2}`;
+  const correctDate = "2024-04-19";
+
   const errorMessage = document.getElementById("errorMessage");
 
-  const correctDate = "2024-07-19"; // ganti sesuai tanggal kamu
-
-  if (dateInput === correctDate) {
-    window.location.href = "flower.html";
+  if (inputDate === correctDate) {
+    confetti(); // panggil efek confetti 🎉
+    setTimeout(() => {
+      window.location.href = "anniv.html";
+    }, 1500); // beri waktu confetti muncul sebelum pindah
   } else {
-    errorMessage.textContent = "🌧️ Wah, tanggalnya belum tepat. Coba lagi ya!";
+    errorMessage.textContent = "😢 Tanggalnya salah, coba lagi yuk~";
   }
 }
